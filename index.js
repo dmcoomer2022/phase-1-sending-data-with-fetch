1b67cd93c777cde883d9e2353394564d29a038b7
David Coomer <dmcoomer@hotmail.com>
2022-05-12 17:47:17 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+
+
 // Add your code here
+function submitData(name, email) {
+    const formData = {
+        name: name,
+        email: email
+      };
+      
+      const configurationObject = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+        body: JSON.stringify(formData),
+      };
+      
+      return fetch("http://localhost:3000/users", configurationObject)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (object) {
+          console.log(object);
+          document.body.append(object.id);
+        })
+        .catch(function (error) {
+            alert(error.message);
+            console.log(error.message);
+            document.body.append(error.message);
+        });
+}
